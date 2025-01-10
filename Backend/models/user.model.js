83c0706117bccf2d9ea2b4e9eee2006bcbce6599
AliRaza192 <alirazaqasim192@gmail.com>
2025-01-10 22:15:03 +0500

    add user model

---
+++ Backend/models/user.model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+// import * as jwt from "jsonwebtoken";
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: [3, "First Name must be at least 5 characters long"],
+    },
+    lastName: {
+      type: String,
+      minlength: [3, "Last Name must be at least 5 characters long"],
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: [5, "Email must be at least 5 characters long"],
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  socketId: {
+    type: String,
+  },
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+  return token;
+};
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashedPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
